77d8f4ba056a89a28a1cba3fb562c5aeebdba97c
addshore <addshorewiki@gmail.com>
2014-11-12 14:08:05 +0000

    Refactor HtmlGenerator into smaller methods

--- assignment/src/HtmlGenerator.php
+++ assignment/src/HtmlGenerator.php
@@ -14,11 +14,29 @@ class HtmlGenerator {
 	 */
 	public function generate( array $champions, array $wikidataItems = array() ) {
 		$dom = new DOMDocument();
+		$dom->formatOutput = true;
 
+		$this->appendHtmlToDom( $dom, $champions, $wikidataItems );
+
+		return '<!DOCTYPE html>' . "\n" . $dom->saveHTML();
+	}
+
+	/**
+	 * Append HTML to the DOM
+	 */
+	private function appendHtmlToDom( DOMDocument $dom, array $champions, array $wikidataItems ) {
 		$html = $dom->appendChild( $dom->createElement( 'html' ) );
 		/** @var DOMElement $html */
 		$html->setAttribute( 'lang', 'en' );
 
+		$this->appendHeadToNode( $dom, $html );
+		$this->appendBodyToNode( $dom, $html, $champions, $wikidataItems );
+	}
+
+	/**
+	 * Append HEAD to HTML
+	 */
+	private function appendHeadToNode( DOMDocument $dom, DOMNode $html ) {
 		$head = $html->appendChild( $dom->createElement( 'head' ) );
 
 		$charset = $head->appendChild( $dom->createElement( 'meta' ) );
@@ -34,7 +52,17 @@ table, td, th {
     border: 1px solid grey;
 }
 ') );
+	}
 
+	/**
+	 * Append BODY to HTML
+	 *
+	 * @param DOMDocument $dom
+	 * @param DOMNode $html
+	 * @param Champion[] $champions
+	 * @param Item[] $wikidataItems
+	 */
+	private function appendBodyToNode( DOMDocument $dom, DOMNode $html, $champions, $wikidataItems ) {
 		$body = $html->appendChild( $dom->createElement( 'body' ) );
 		$body->appendChild( $dom->createElement( 'h3', 'Chess World Champions (1886–2013)' ) );
 
@@ -45,80 +73,115 @@ table, td, th {
 		$tr1->appendChild( $dom->createElement( 'th', 'Country' ) );
 
 		foreach( $champions as $champion ) {
-			/** @var DOMElement $tr */
-			$tr = $table->appendChild( $dom->createElement( 'tr' ) );
-			$tr->setAttribute( 'itemscope', '' );
-			$tr->setAttribute( 'itemtype', 'http://schema.org/Person' );
+			//TODO fill $item with the wikidata item if there is one
+			$item = null;
+			$this->appendChampionRowToNode( $dom, $table, $champion, $item );
+		}
+	}
 
-			$name = $tr->appendChild( $dom->createElement( 'td' ) );
-			if( !is_null( $champion->getEnwikilink() ) ) {
-				/** @var DOMElement $nameLink */
-				$nameLink = $name->appendChild( $dom->createElement( 'a', $champion->getName() ) );
-				$nameLink->setAttribute( 'href', $champion->getEnwikilink() );
-				$nameLink->setAttribute( 'title', $champion->getName() );
-				$nameLink->setAttribute( 'itemprop', 'name' );
-			} else {
-				$name->appendChild( $dom->createTextNode( $champion->getName() ) );
-			}
+	/**
+	 * Append a ROW for a TABLE
+	 *
+	 * @param DOMDocument $dom
+	 * @param DOMNode $table
+	 * @param Champion $champion
+	 * @param Item|null $item
+	 */
+	private function appendChampionRowToNode( DOMDocument $dom, DOMNode $table, $champion, $item = null ) {
+		/** @var DOMElement $tr */
+		$tr = $table->appendChild( $dom->createElement( 'tr' ) );
+		$tr->setAttribute( 'itemscope', '' );
+		$tr->setAttribute( 'itemtype', 'http://schema.org/Person' );
 
+		$this->appendNameColToNode( $dom, $tr, $champion );
+		$this->appendYearColToNode( $dom, $tr, $champion );
+		$this->appendCountryColToNode( $dom, $tr, $champion );
+	}
 
-			$year = $tr->appendChild( $dom->createElement( 'td' ) );
-			foreach( $champion->getReigns() as $reign ) {
-				$year->appendChild( $dom->createTextNode( $reign->getStartYear() . '-' . $reign->getEndYear() ) );
-				if( !is_null( $reign->getType() ) ) {
-					$year->appendChild( $dom->createTextNode( ' (' . $reign->getType() . ')' ) );
-				}
-				$year->appendChild( $dom->createElement( 'br' ) );
-			}
-			//Remove the last br tag
-			$year->removeChild( $year->lastChild );
-
-			$country = $tr->appendChild( $dom->createElement( 'td' ) );
-			$lastCountryLinkNode = null;
-			foreach( $champion->getLocations() as $location ) {
-				if( !is_null( $location->getFlagUrl() ) ) {
-					/** @var DOMElement $flagImg */
-					$flagImg = $country->appendChild( $dom->createElement( 'img' ) );
-					$flagImg->setAttribute( 'alt', $location->getCountry() . ' flag' );
-					$flagImg->setAttribute( 'src', $location->getFlagUrl() );
-					$flagImg->setAttribute( 'width', 30 );
-				} else {
-					die();//TODO do something if there isnt a flag...
-				}
-				$country->appendChild( $dom->createTextNode( ' ' ) );
-
-				if( !is_null( $location->getCountryLink() ) ) {
-					/** @var DOMElement $countryLink */
-					$countryLink = $country->appendChild( $dom->createElement( 'a', $location->getCountry() ) );
-					$countryLink->setAttribute( 'href', $location->getCountryLink() );
-					$countryLink->setAttribute( 'title', $location->getCountry() );
-					$lastCountryLinkNode = $countryLink;
-				} else {
-					$country->appendChild( $dom->createTextNode( $location->getCountry() ) );
-				}
-				if( !is_null( $location->getHistorical() ) ) {
-					$country->appendChild( $dom->createTextNode( ' (' ) );
-					if( !is_null( $location->getHistoricalLink() ) ) {
-						/** @var DOMElement $historicalLink */
-						$historicalLink = $country->appendChild( $dom->createElement( 'a', $location->getHistorical() ) );
-						$historicalLink->setAttribute( 'href', $location->getHistoricalLink() );
-						$historicalLink->setAttribute( 'title', $location->getHistorical() );
-					} else {
-						$country->appendChild( $dom->createTextNode( $location->getHistorical() ) );
-					}
-					$country->appendChild( $dom->createTextNode( ')' ) );
-				}
-				$country->appendChild( $dom->createElement( 'br' ) );
-			}
-
-			if( !is_null( $lastCountryLinkNode ) ) {
-				$lastCountryLinkNode->setAttribute( 'itemprop', 'nationality' );
-			}
+	/**
+	 * Append some ROW DATA to a ROW
+	 *
+	 * @param DOMDocument $dom
+	 * @param DOMNode $tr
+	 * @param Champion $champion
+	 */
+	private function appendNameColToNode( DOMDocument $dom, DOMNode $tr, Champion $champion ) {
+		$name = $tr->appendChild( $dom->createElement( 'td' ) );
+		if( !is_null( $champion->getEnwikilink() ) ) {
+			/** @var DOMElement $nameLink */
+			$nameLink = $name->appendChild( $dom->createElement( 'a', $champion->getName() ) );
+			$nameLink->setAttribute( 'href', $champion->getEnwikilink() );
+			$nameLink->setAttribute( 'title', $champion->getName() );
+			$nameLink->setAttribute( 'itemprop', 'name' );
+		} else {
+			$name->appendChild( $dom->createTextNode( $champion->getName() ) );
+		}
 
 	}
 
-		$dom->formatOutput = true;
-		return '<!DOCTYPE html>' . "\n" . $dom->saveHTML();
+	/**
+	 * Append some ROW DATA to a ROW
+	 *
+	 * @param DOMDocument $dom
+	 * @param DOMNode $tr
+	 * @param Champion $champion
+	 */
+	private function appendYearColToNode( DOMDocument $dom, DOMNode $tr, Champion $champion ) {
+		$year = $tr->appendChild( $dom->createElement( 'td' ) );
+		foreach( $champion->getReigns() as $reign ) {
+			$year->appendChild( $dom->createTextNode( $reign->getStartYear() . '-' . $reign->getEndYear() ) );
+			if( !is_null( $reign->getType() ) ) {
+				$year->appendChild( $dom->createTextNode( ' (' . $reign->getType() . ')' ) );
+			}
+			$year->appendChild( $dom->createElement( 'br' ) );
+		}
+		//Remove the last br tag
+		$year->removeChild( $year->lastChild );
+	}
+
+	/**
+	 * Append some ROW DATA to a ROW
+	 *
+	 * @param DOMDocument $dom
+	 * @param DOMNode $tr
+	 * @param Champion $champion
+	 */
+	private function appendCountryColToNode( DOMDocument $dom, DOMNode $tr, Champion $champion ) {
+		$country = $tr->appendChild( $dom->createElement( 'td' ) );
+		foreach( $champion->getLocations() as $location ) {
+			if( !is_null( $location->getFlagUrl() ) ) {
+				/** @var DOMElement $flagImg */
+				$flagImg = $country->appendChild( $dom->createElement( 'img' ) );
+				$flagImg->setAttribute( 'alt', $location->getCountry() . ' flag' );
+				$flagImg->setAttribute( 'src', $location->getFlagUrl() );
+				$flagImg->setAttribute( 'width', 30 );
+			} else {
+				die();//TODO do something if there isnt a flag...
+			}
+			$country->appendChild( $dom->createTextNode( ' ' ) );
+
+			if( !is_null( $location->getCountryLink() ) ) {
+				/** @var DOMElement $countryLink */
+				$countryLink = $country->appendChild( $dom->createElement( 'a', $location->getCountry() ) );
+				$countryLink->setAttribute( 'href', $location->getCountryLink() );
+				$countryLink->setAttribute( 'title', $location->getCountry() );
+			} else {
+				$country->appendChild( $dom->createTextNode( $location->getCountry() ) );
+			}
+			if( !is_null( $location->getHistorical() ) ) {
+				$country->appendChild( $dom->createTextNode( ' (' ) );
+				if( !is_null( $location->getHistoricalLink() ) ) {
+					/** @var DOMElement $historicalLink */
+					$historicalLink = $country->appendChild( $dom->createElement( 'a', $location->getHistorical() ) );
+					$historicalLink->setAttribute( 'href', $location->getHistoricalLink() );
+					$historicalLink->setAttribute( 'title', $location->getHistorical() );
+				} else {
+					$country->appendChild( $dom->createTextNode( $location->getHistorical() ) );
+				}
+				$country->appendChild( $dom->createTextNode( ')' ) );
+			}
+			$country->appendChild( $dom->createElement( 'br' ) );
+		}
 	}
 
 } 
